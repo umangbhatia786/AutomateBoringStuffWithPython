0a79038334ab4ac51513f889ac9bc5ee17875d91
Umang Bhatia <umangbhatia786@gmail.com>
2020-03-05 12:46:13 +0530

    Update PyValidatorGUI.py

--- Provident/PyValidatorGUI.py
+++ Provident/PyValidatorGUI.py
@@ -1,42 +1,42 @@
-import PySimpleGUI as sg
-
-def print1():
-    print('Generate Logs Clicked')
-
-def print2():
-    print('Quit Clicked')
+import PySimpleGUI as sg 
+import psutil
+from Validator import *
 
 
 sg.theme('Light Blue 2')
 
 layout = [[sg.Text('Choose files to get started', size=(30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],
           [sg.Text('Request File', size=(15,1)), sg.Input(key='req'), sg.FileBrowse()],
-          [sg.Text('SVAS Log File', size=(15,1)), sg.Input(), sg.FileBrowse()],
-          [sg.Text('HSS Log File', size=(15,1)), sg.Input(), sg.FileBrowse()],
-          [sg.Submit('Generate Logs'), sg.Cancel('Quit'), sg.Button('Reset')],
-          [sg.Txt('', size=(8,1), key='output')]]
-
-window = sg.Window('Provident Logs Validator', layout, size=(800,200))
-
-event, values = window.read()
-'''window.close()
-print(f'You clicked {event}')
-print(f'You chose filenames {values[0]} and {values[1]}')'''
+          [sg.Text('SVAS Log File', size=(15,1)), sg.Input(key='svas'), sg.FileBrowse()],
+          [sg.Text('HSS Log File', size=(15,1)), sg.Input(key='hss'), sg.FileBrowse()],
+          [sg.Submit('Generate Logs'), sg.Cancel('Quit'), sg.Button('Reset')]]
 
 
+window = sg.Window('Provident Logs Validator', layout, size=(600,300))
 
 while True:
     event, values = window.read()
+  
     if event in (None, 'Quit'):
         window.close()
+        for proc in psutil.process_iter():
+            if proc.name() == 'ValidatorGUI.exe':
+                proc.kill()
+                
     elif event == 'Generate Logs':
-        print(event)
-        print1()
-        print(values[0])
-        print(values[1])
-        print(values[2])
+        try:
+            validator(values)
+            sg.popup('Logs Generated Succesfully!!')
+        except Exception as e:
+            sg.popup("Error - Select all the files again")
+        finally:
+            window['req']('')
+            window['svas']('')
+            window['hss']('')
+            values = {}
 
     elif event == 'Reset':
-        window['req'].update('')
-
-
+        window['req']('')
+        window['svas']('')
+        window['hss']('')
+        values = {}
